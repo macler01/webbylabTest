1a8b157cf34d27b9fe39febafa698326218b87e7
macler01 <vasilenko.maxim5@gmail.com>
2020-12-10 00:53:00 +0200

    init heroku

---
+++ routes/filmsRouter.js
@@ -0,0 +1,9 @@
+const router = require('express').Router;
+
+router.post(`/`, async () => {
+
+});
+
+router.get(`/`, async () => {
+
+})
---
+++ server.js
@@ -0,0 +1,14 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get(`/`, (req,res,next) => {
+    res.send('Test router');
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app is listening on port http://localhost:${PORT}`);
+})
