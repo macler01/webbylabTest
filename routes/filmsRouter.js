5a044769e6e2492718927344c2e1c4e214ed6f8c
macler01 <vasilenko.maxim5@gmail.com>
2020-12-10 23:46:15 +0200

    db

--- routes/filmsRouter.js
+++ routes/filmsRouter.js
@@ -1,7 +1,18 @@
 const router = require('express').Router;
+const db = require('./models/index.js');
 
-router.post(`/`, async () => {
+router.post(`/`, async (req,res,next) => {
+    let params = req.body;
+    let dbResponse = await db.Films.findOrCreate(
+        {where: {
+            name: params.name,
+            year: params.year,
+            format:
+        }
+    });
+    console.log(dbResponse);
 
+    
 });
 
 router.get(`/`, async () => {
