e901f8c636802c86e2a2577f15e6fac2d09c45b6
macler01 <vasilenko.maxim5@gmail.com>
2020-12-11 21:46:59 +0200

    add routers

--- routes/filmsRouter.js
+++ routes/filmsRouter.js
@@ -21,4 +21,25 @@ router.get(`/all`, async (req,res,next) => {
     return res.status(200).json(films);
 });
 
+router.get(`/filter/:filter/:value`,async (req,res,next) => {
+    let filter = req.params.filter;
+    let value = req.params.value;
+    let where = {};
+    where[filter] = value;
+    let film = await db.Films.findAll({
+        where: where
+      });
+    return res.status(200).json(film);  
+})
+
+router.delete(`/id/:id`, async (req,res,next) => {
+    let id = params.id;
+    await db.Films.destroy({
+        where: {
+          id: id
+        }
+    });
+    return res.status(200).send(`OK`);
+});
+
 module.exports = router;
