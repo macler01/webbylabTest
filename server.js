f366beacc757f7e88ba8c97cf6816fa0967abc78
macler01 <vasilenko.maxim5@gmail.com>
2020-12-16 00:59:12 +0200

    fix

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
-const exphbs         = require('express-handlebars');
 const PORT           = process.env.PORT || 3000;
 const filmRouter     = require(`./routes/filmsRouter`);
 
-// View
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 // Parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
